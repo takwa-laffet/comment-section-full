752fc1807a263d38d0672225e839acbb08e7ebc7
Hamdi Kamel <hamdiakippuden@gmail.com>
2025-09-20 17:07:50 +0100

    created the sign in component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,26 @@ import CommentList from './components/CommentList'
 
 export const StateContext = createContext()
 
+// keeping it here to access it later
+const SignIn = () => {
+  return (
+    <div className="sign-in">
+      <form action="#">
+        <fieldset>
+          <label htmlFor="email">Email:</label>
+          <input type="text" name='email' id='email' />
+        </fieldset>
+        
+        <fieldset>
+          <label htmlFor="password">Password:</label>
+          <input type="text" name='password' id='password' />
+        </fieldset>
+        <button>Sign In</button>
+      </form>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
