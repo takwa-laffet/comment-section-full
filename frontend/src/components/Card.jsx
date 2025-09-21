1188d4d1a0fd18bc003aa831feed4f5f83a39032
Hamdi Kamel <hamdiakippuden@gmail.com>
2025-09-21 09:19:41 +0100

    extract score logic into a separate subcomponent

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -3,36 +3,45 @@ import { StateContext } from "../context"
 import { useRef } from "react"
 import UserProfile, { UserActions } from "./UserProfile"
 
+const ScoreComponent = ({ score, itemId }) => {
+  const {actions} = useContext(StateContext)
+  const currentScoreRef = useRef(score)
+
+  const handleIncrementScoreClick = () =>
+    actions.scoreIncremented(itemId, currentScoreRef.current)
+
+  const handleDecrementScoreDispatch = () =>
+    actions.scoreDecremented(itemId, currentScoreRef.current)
+
+  return (
+    <div className="score-component">
+      <button onClick={handleIncrementScoreClick}>
+        <div className="icon-img">
+          <img src="/images/icon-plus.svg" alt="" />
+        </div>
+      </button>
+
+      <span>{score}</span>
+
+      <button onClick={handleDecrementScoreDispatch}>
+        <div className="icon-img">
+          <img src="/images/icon-minus.svg" alt="" />
+        </div>
+      </button>
+    </div>
+  )
+}
+
 // This component extracts presentational logic which keeps both comments and replies visually consistent without needing to know about their differences.
 const Card = ({
   item
 }) => {
-  const {actions} = useContext(StateContext)
-  const currentScoreRef = useRef(item.score)
-
-  const handleIncrementScoreClick = () =>
-    actions.scoreIncremented(item.id, currentScoreRef.current)
-
-  const handleDecrementScoreDispatch = () =>
-    actions.scoreDecremented(item.id, currentScoreRef.current)
-
   return (
     <div className="card">
-      <div className="score-component">
-        <button onClick={handleIncrementScoreClick}>
-          <div className="icon-img">
-            <img src="/images/icon-plus.svg" alt="" />
-          </div>
-        </button>
-
-        <span>{item.score}</span>
-
-        <button onClick={handleDecrementScoreDispatch}>
-          <div className="icon-img">
-            <img src="/images/icon-minus.svg" alt="" />
-          </div>
-        </button>
-      </div>
+      <ScoreComponent
+        score={item.score}
+        itemId={item.id}
+      />
 
       <div className="content">
         <div className="profile-header">
