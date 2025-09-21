9f3e5363fb66f05c9f6690f983def806cbdca9a3
Hamdi Kamel <hamdiakippuden@gmail.com>
2025-09-21 09:07:26 +0100

    removed comment component as it was unnecessary

--- frontend/src/components/CommentList.jsx
+++ frontend/src/components/CommentList.jsx
@@ -2,39 +2,30 @@ import { useContext } from "react"
 import { StateContext } from "../context"
 import Card from "./Card"
 
-const Comment = ({
-  comment
-}) => {
-  const {comments} = useContext(StateContext)
-
-  return (
-    <div className="comment">
-      <Card item={comment} />
-
-      <div className="replies-list">
-        {comment.replies.map(id => (
-          <Card
-            item={comments.byId[id]}
-            key={id}
-          />
-        ))}
-      </div>
-    </div>
-  )
-}
-
 function CommentList() {
   const {comments} = useContext(StateContext)
   const commentIds = comments.allId.filter(id => !comments.byId[id].parentId)
 
   return (
     <div className="comments-list">
-      {commentIds.map(id => (
-        <Comment
-          comment={comments.byId[id]}
-          key={id}
-        />
-      ))}
+      {commentIds.map(id => {
+        const comment = comments.byId[id]
+
+        return (
+          <div className="comment">
+            <Card item={comment} />
+
+            <div className="replies-list">
+              {comment.replies.map(replyId => (
+                <Card
+                  item={comments.byId[replyId]}
+                  key={replyId}
+                />
+              ))}
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
