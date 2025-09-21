3e900b2ce3ccd7e089c80c6cc6a75248e32cc93f
Hamdi Kamel <hamdiakippuden@gmail.com>
2025-09-21 22:55:09 +0100

    replaced comments id property with strings for better security

--- frontend/src/hooks.js
+++ frontend/src/hooks.js
@@ -1,10 +1,13 @@
 import { useReducer } from "react"
 import { reducer } from './reducer'
 import data from './data/comments.json'
+import { v4 as uuidv4 } from 'uuid'
 
 /*
     Just keeping this here for reference. I'm going to define some "abstract" methods that update state so I don't have to track dispatch calls throughout the codebase. The easier it gets to modify the way dispatch calls work, the less time I can spend on making several changes within components that shouldn't care how state gets updated.
 */
+
+console.log(uuidv4())
 
 export function useComments() {
     const [comments, dispatch] = useReducer(reducer, data)
