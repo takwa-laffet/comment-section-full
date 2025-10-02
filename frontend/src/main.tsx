8183bf26659a23b70522eff57310c343e7405522
Hamdi Kamel <hamdiakippuden@gmail.com>
2025-10-02 13:17:07 +0100

    refactor: reorganize CSS file structure and update import path in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter as Router} from 'react-router-dom'
 import App from './App'
-import './index.css'
+import './css/index.css'
 
 createRoot(document.getElementById('root') as HTMLDivElement).render(
   <StrictMode>
